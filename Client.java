491a32435b11317c8ad2573b414bba7a91caa74d
sean cork <seantcork@gmaol.com>
2019-02-25 23:24:10 -0500

    i think its working

--- Client.java
+++ Client.java
@@ -52,8 +52,8 @@ public class Client {
       scanner = new Scanner(System.in);
       request = scanner.nextLine();
       String[] line = request.split(" ");
-      params.add(line[0])
-      params.add(line[1])
+      params.add(line[0]);
+      params.add(line[1]);
 
       
       try {
