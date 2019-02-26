4380c4335a773a83207a8f3f2b4c4b8eed6eb1ea
sean cork <seantcork@gmaol.com>
2019-02-25 23:19:56 -0500

    i think its working

--- Front.java
+++ Front.java
@@ -34,7 +34,7 @@ public class Front {
 
     if(function.equals("search")){
       try {
-        result = client.execute("Catalog.query_by_topic", params);
+        result = (String) client.execute("Catalog.query_by_topic", params);
       } catch (Exception e) {
         System.err.println("Client exception: " + e);
       }
@@ -43,7 +43,7 @@ public class Front {
 
     else if(function.equals("lookup")){
       try {
-        result = client.execute("Catalog.query_by_item", params);
+        result = (String) client.execute("Catalog.query_by_item", params);
       } catch (Exception e) {
         System.err.println("Client exception: " + e);
       }
