a96e726864a8ecb72023703a282f50c508a0e6a4
sean cork <seantcork@gmaol.com>
2019-02-28 16:50:20 -0500

    last things

--- Front.java
+++ Front.java
@@ -15,11 +15,11 @@ public class Front {
   static String order_server = "";
   static String catalog_server = "";
 
-  XmlRpcClient new_client(String port_num) {
+  XmlRpcClient new_client(String port_num, String server) {
     XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
     XmlRpcClient client = null;
     try {
-      config.setServerURL(new URL("http://" + catalog_server + ":" + port_num));
+      config.setServerURL(new URL("http://" + server + ":" + port_num));
       client = new XmlRpcClient();
       client.setConfig(config);
     } catch (Exception e) {
@@ -38,7 +38,7 @@ public class Front {
     if(function.equals("search")){
       Object[] books;
       String reply = "";
-      XmlRpcClient client = new_client("8123");
+      XmlRpcClient client = new_client("8123", catalog_server);
       try {
         books = (Object[]) client.execute("Catalog.query_by_topic", params);
       } catch (Exception e) {
@@ -54,7 +54,7 @@ public class Front {
     else if(function.equals("lookup")) {
       Object[] bookinfo;
       String reply = "";
-      XmlRpcClient client = new_client("8123");
+      XmlRpcClient client = new_client("8123", catalog_server);
       try {
         bookinfo = (Object[]) client.execute("Catalog.query_by_item", params);
       } catch (Exception e) {
@@ -79,7 +79,7 @@ public class Front {
 
     else if(function.equals("buy")) {
       Object confirmation;
-      XmlRpcClient client = new_client("8125");
+      XmlRpcClient client = new_client("8125", order_server);
       try {
         confirmation = client.execute("Order.buy", params);
       } catch (Exception e) {
